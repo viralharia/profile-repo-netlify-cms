e97bcb9f5dffdd6ca5ce62647b948f48a180fa95
vharia <viralhar@gmail.com>
2019-11-06 12:54:23 +0530

    updated navbar items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -62,24 +62,19 @@ const Navbar = class extends React.Component {
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
                 About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
+              </Link>              
               <Link className="navbar-item" to="/blog">
-                Blog
+                Blog/Diary
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
+              
             </div>
             <div className="navbar-end has-text-centered">
               <a
                 className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
+                href="https://github.com/viralharia"
                 target="_blank"
                 rel="noopener noreferrer"
               >
